b2d44a0017ad4b9f12f3c01a30dc58747042b860
Cosme Alves <cosme.alex@gmail.com>
2022-11-22 12:00:12 -0300

    New Feturare

--- app/auth.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth import get_user_model
-from django.db.models import Q
-
-def Email_OR_Username():
-  def get_user(self, user_id):
-    try:
-      
